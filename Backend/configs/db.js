c7d25d495517e2b6a03a7d6c3654b1706610f0db
vamshi951 <167439235+vamshi951@users.noreply.github.com>
2026-03-27 02:57:07 +0530

    Refactor MONGO_URI error logging in db.js

--- Backend/configs/db.js
+++ Backend/configs/db.js
@@ -4,15 +4,8 @@ const mongoose = require("mongoose");
 
 const uri = process.env.MONGO_URI;
 
-<<<<<<< HEAD
-console.log("DEBUG URI:", uri); // 👈 check
-
-if (!uri) {
-  console.log("❌ MONGO_URI is undefined");
-=======
 if (!uri) {
   console.log("❌ MONGO_URI missing");
->>>>>>> ebd87bc8bda2b5cbcb71370591c8836f1c38b975
   process.exit(1);
 }
 
