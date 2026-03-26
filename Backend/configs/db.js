0d5a7d1db24c6604ecc4879159a649e10cc0ee8c
vamshi951 <167439235+vamshi951@users.noreply.github.com>
2026-03-27 00:26:33 +0530

    Update MongoDB connection and export

--- Backend/configs/db.js
+++ Backend/configs/db.js
@@ -1,7 +1,7 @@
-const mongoose = require("mongoose");
 require("dotenv").config();
-console.log("MONGO_URI:", process.env.MONGO_URI);
 
-const connection = mongoose.connect(process.env.dbURL);
+const mongoose = require("mongoose");
 
-module.exports = { connection };
+const connection = mongoose.connect(process.env.MONGO_URI);
+
+module.exports = connection;
